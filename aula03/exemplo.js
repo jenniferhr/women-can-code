626d5192e16d52269452944de5e1f28bc8338314
Jennifer Helen <jenniferh7637@gmail.com>
2021-11-11 17:20:06 -0300

    Add files via upload

---
+++ aula03/exemplo.js
@@ -0,0 +1,6 @@
+const nome = "Jennifer Helen"
+const idade = 20
+const eMaiorDeIdade = true
+
+console.log("Meu nome é " + nome + " e eu tenho " + idade + " anos")
+console.log(`Meu nome é ${nome} e eu tenho ${idade} anos`)
---
+++ aula03/exercicio5.js
@@ -0,0 +1,8 @@
+let soma = 3 + 7
+let subtracao = 135 - 29
+let multiplicacao = 4 * 8
+let divisao = 225 / 5
+console.log(soma)
+console.log(subtracao)
+console.log(multiplicacao)
+console.log(divisao)
